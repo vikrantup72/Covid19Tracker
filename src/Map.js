21abc181f59cbd708ab4f48de7cae807354e5cc1
vikrantup72 <vikraantup72@gmail.com>
2020-09-20 13:37:52 -0400

    make it better

--- src/Map.js
+++ src/Map.js
@@ -9,7 +9,7 @@ function Map({ countries, casesType, center, zoom }) {
       <LeafletMap center={center} zoom={zoom}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">vikrantup72</a> contributors'
+          attribution='&copy; <a href="https://vikrantup72.netlify.app/">vikrantup72</a> contributors'
         />
         {showDataOnMap(countries, casesType)}
       </LeafletMap>
